d6056eabaf287e7ec8010961bebf2ac7036c46e6
keyxcode <keyxcode@gmail.com>
2023-02-23 13:39:22 +0700

    Implement a basic add book function for console usage

--- books.js
+++ books.js
@@ -1,3 +1,7 @@
+const myLib = [];
+
+const bookList = document.querySelector("#book-list");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -9,9 +13,20 @@ function Book(title, author, pages, read) {
   //   };
 }
 
-Book.prototype.info = function () {
-  return `${title} by ${author}, ${pages} pages, ${this.read}`;
-};
+// Book.prototype.info = function () {
+//   return `${title} by ${author}, ${pages} pages, ${this.read}`;
+// };
 
-const book1 = new Book("doremon", "fujiko", 100, true);
-const book2 = new Book("naruto", "fujiko", 100, false);
+function render(book) {
+  const node = document.createElement("li");
+  node.innerText = Object.values(book);
+  bookList.appendChild(node);
+}
+
+function addBookToLib(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
+  render(book);
+}
+
+// const book1 = new Book("doremon", "fujiko", 100, true);
+// const book2 = new Book("naruto", "fujiko", 100, false);
